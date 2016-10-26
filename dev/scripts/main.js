508e268bfb7957cb9a4716b9b14bb67cec30ffb8
Erin Wise <erinannewise@gmail.com>
2016-10-26 15:14:49 -0400

    do not accept my changes

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -9,15 +9,16 @@ function getLocation() {
     }
 }
 
-var map = L.map('map', {
-	center: [coordinate1, coordinate2],
-	zoom: 13
-});
-	L.titleLayer('http://{s}.title.openstreetmap.org/{z}/{x}/{y}.png',
-		attribution: 'Map data &copy; <a href="http: THE REST'
-		maxZoom: 18
-	}).addTo(map);
-});
+ myApp.findCity = function(lat,lng){
+         $.ajax({
+             url: `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyD3sqMdCuQ0LelH9L1ox8A7vw5JPFOAuGw`,
+             dataType: 'JSON',
+             method: 'POST',
+         }).then(function(cityData){
+             console.log(cityData);
+             return cityData[0].cityName
+         });
+        }
 
 function showPosition(position) {
      userLat =  Math.position.coords.latitude; 
@@ -25,3 +26,30 @@ function showPosition(position) {
      console.log("this is users Lat" + userLat);
      console.log("this is users long" + userLong);
 }
+
+
+// var map = L.map('map', {
+//     center: [coordinate1, coordinate2],
+//     zoom: 13
+// });
+//     L.titleLayer('http://{s}.title.openstreetmap.org/{z}/{x}/{y}.png',
+//         attribution: 'Map data &copy; <a href="http: THE REST'
+//         maxZoom: 18
+//     }).addTo(map);
+// });
+
+
+// function getLocation() {
+//         if (navigator.geolocation) {
+//             navigator.geolocation.getCurrentPosition(showPosition);
+//         } else {
+//             alert('browser does not support location services.');
+//         }
+//     }
+function showPosition(position) {
+        lat = position.coords.latitude; 
+        lng = position.coords.longitude; 
+        console.log(lat);
+        console.log(lng);
+        myApp.findCity(lat,lng);
+    }
